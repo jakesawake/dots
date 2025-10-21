8de381be1d8b00b86ba4883144de014c4bd78c9a
Jake Canlas <jakechristiancanlas1@gmail.com>
2025-10-21 19:27:44 -0400

    Made changes to LSP config
    - made changes to LSP config after Joseans new video on the new way to
    setup/configure LSP's in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,3 @@
 require("jake.core")
 require("jake.lazy")
+require("jake.lsp")
--- .config/nvim/lua/jake/plugins/nvim-cmp.lua
+++ .config/nvim/lua/jake/plugins/nvim-cmp.lua
@@ -26,9 +26,6 @@ return {
     require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
       snippet = { -- configure how nvim-cmp interacts with snippet engine
         expand = function(args)
           luasnip.lsp_expand(args.body)
@@ -45,11 +42,12 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp"},
+        { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
+
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
