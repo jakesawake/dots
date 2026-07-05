8afab63fa0c138e83f762f7d2e967d63a36830cb
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-07-05 16:56:25 -0400

    Fixed warning errors for ESLint library

--- .config/nvim/after/lsp/eslint.lua
+++ .config/nvim/after/lsp/eslint.lua
@@ -1,3 +1,11 @@
 return {
-  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+  -- html/css removed: ESLint doesn't lint those, and attaching caused
+  -- "[lspconfig] Unable to find ESLint library" warnings on every open
+  filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact", "svelte" },
+  handlers = {
+    -- silently ignore "no local eslint found" instead of showing a warning
+    ["eslint/noLibrary"] = function()
+      return {}
+    end,
+  },
 }
