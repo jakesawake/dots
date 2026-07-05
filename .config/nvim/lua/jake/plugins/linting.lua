0cfefff9ef56fa3d4245521ea4382c7b00078592
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-07-05 16:59:44 -0400

    Skipped eslint_d if no eslint config exists

--- .config/nvim/lua/jake/plugins/linting.lua
+++ .config/nvim/lua/jake/plugins/linting.lua
@@ -18,6 +18,18 @@ return {
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
+        -- skip eslint_d when no eslint config exists in the project tree;
+        -- without this check, eslint_d errors with "Could not find config file"
+        -- and nvim-lint surfaces it as a parse error diagnostic on line 1
+        local eslint_fts = { javascript = true, typescript = true, javascriptreact = true, typescriptreact = true, svelte = true }
+        if eslint_fts[vim.bo.filetype] then
+          local has_config = #vim.fs.find({
+            ".eslintrc", ".eslintrc.js", ".eslintrc.cjs",
+            ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json",
+            "eslint.config.js", "eslint.config.mjs", "eslint.config.cjs",
+          }, { path = vim.fn.expand("%:p:h"), upward = true }) > 0
+          if not has_config then return end
+        end
         lint.try_lint()
       end,
     })
