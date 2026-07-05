de01fd61b45792bdb78a78e7fa42de86cf8d4ebe
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-07-05 17:00:55 -0400

    Misc changes

--- .config/nvim/lua/jake/lazy.lua
+++ .config/nvim/lua/jake/lazy.lua
@@ -11,12 +11,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{ import = "jake.plugins"}, {import = "jake.plugins.lsp"}}, {
+require("lazy").setup({ { import = "jake.plugins" }, { import = "jake.plugins.lsp" } }, {
+  defaults = {
+    lazy = false,
+    version = "*",
+  },
   checker = {
     enabled = true,
     notify = false,
   },
   change_detection = {
-  notify = false,
+    notify = false,
   },
 })
