bb76f6eb11778a38b84eb501f1d20cb1ce9597a9
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-08-08 21:26:30 -0400

    Added auto center for jumping when using arrow

--- .config/nvim/lua/jake/plugins/arrow.lua
+++ .config/nvim/lua/jake/plugins/arrow.lua
@@ -4,11 +4,24 @@
 return {
   "otavioschwanck/arrow.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    show_icons = true,
-    -- leader_key must be a single key; <leader>j style sequences break the internal toggle
-    leader_key = ";",
-    buffer_leader_key = "m",
-    save_on_toggle = true,
-  },
+  config = function()
+    require("arrow").setup({
+      show_icons = true,
+      -- leader_key must be a single key; <leader>j style sequences break the internal toggle
+      leader_key = ";",
+      buffer_leader_key = "m",
+      save_on_toggle = true,
+      after_jump_callback = function()
+        vim.cmd("normal! zz")
+      end,
+    })
+
+    -- buffer bookmark jumps fire this event; hook it to always center
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "ArrowGoToBookmark",
+      callback = function()
+        vim.cmd("normal! zz")
+      end,
+    })
+  end,
 }
